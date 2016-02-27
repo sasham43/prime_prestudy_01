1498c0c7c9de716f40af07330c8daa4777bce349
sasham43 <sashasemail@gmail.com>
2016-02-27 15:40:29 -0500

    Add ability to select items by clicking on them

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function message_generator(message){
 }
 
 message_generator(global_message);
+
+// color depositor
+
+function clicked(id){
+  document.querySelector(id).classList.toggle("selected");
+}
