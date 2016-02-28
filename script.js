2214b1ae75133d9eeeb2a60aefa2151be33c1cac
sasham43 <sashasemail@gmail.com>
2016-02-28 12:19:00 -0500

    Finally found a better way to get the id of the clicked element, and improved box selection

--- script.js
+++ script.js
@@ -8,6 +8,33 @@ message_generator(global_message);
 
 // color depositor
 
-function clicked(id){
-  document.querySelector(id).classList.toggle("selected");
+function clicked(){
+  var paletteBoxes = document.querySelector(".palette").children;
+
+  for(var i = 0; i < paletteBoxes.length; i++){
+    if (paletteBoxes[i].id == event.target.id){
+      paletteBoxes[i].classList.toggle("selected");
+    }
+    else{
+      paletteBoxes[i].classList.remove("selected");
+    }
+  }
+
+  console.log(event.target.hasAttribute("selected"));
+
 }
+
+
+var paletteBoxes = document.querySelectorAll(".box");
+
+console.log(paletteBoxes);
+
+
+// for(var i=0; i<paletteBoxes.length; i++){
+//   console.log(paletteBoxes[i].classList)
+// }
+
+//stupid jquery
+// $('.palette').click(function(event){
+//   alert(event.target.id);
+// })
